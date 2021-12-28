d79bd78cf718974cd5b43f9089fe3f0330f71967
rohan <ronnie2255@icloud.com>
2021-12-28 12:16:56 -0500

    Maps SDK

--- matrix-routing-app/src/App.js
+++ matrix-routing-app/src/App.js
@@ -1,11 +1,24 @@
-
+import { useEffect, useState } from "react";
 import './App.css';
 
 
 const App = () => {
+
+  const [map, setMap] = useState({})
+
+  useEffect(() => {
+    let map = tt.map({
+      key: process.env.REACT_APP_TOM_TOM_API_KEY,
+      container: mapElement.current,
+  });
+
+  setMap(map);
+    
+  }, [])
+
   return (
     <div className="App">
-      
+      Hello World!! 
     </div>
   )
 }
